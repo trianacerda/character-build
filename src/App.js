de9bc4d7d1abdaa1831e6e981b617f34759b4d98
Triana Cerda <trianaRcerda@gmail.com>
2021-11-30 14:36:34 -0800

    changed prettier + eslint files

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import logo from './logo.svg'
-import './App.css'
-import { useState } from 'React'
+import './App.css';
+import { useState } from 'react';
+import Picker from './components/Picker/Picker.jsx';
 
 function App() {
-  const [head, setHead] = useState('')
+  const [head, setHead] = useState('');
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1 id="triana">
-          <div value="text"></div>
-        </h1>
+        <h1> Character Builder</h1>
+        {/* <Picker /> */}
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Picker/Picker.jsx
@@ -0,0 +1,7 @@
+// import React from 'react';
+
+// function Picker() {
+//   return (  );
+// }
+
+// export default Picker;
