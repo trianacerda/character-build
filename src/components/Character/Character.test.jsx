be373acbbec534b3e0ec4d42662272b603c1f81f
Triana Cerda <trianaRcerda@gmail.com>
2021-11-30 18:11:04 -0800

    code clean up

--- src/components/Character/Character.test.jsx
+++ src/components/Character/Character.test.jsx
@@ -4,7 +4,6 @@ import Character from './Character';
 it('should render Character element on page', async () => {
   const { container } = render(<Character head="beth" middle="cowboy" bottom="gummybear" />);
   const element = await screen.findAllByRole('article');
-  console.log('el', element[0]);
 
   expect(element[0]).toBeInTheDocument();
   expect(container).toMatchSnapshot();
